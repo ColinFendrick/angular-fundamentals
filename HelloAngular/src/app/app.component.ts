b801d50b562ea5c227eebdde166ac9ff0f78a6c8
Colin Fendrick <colin.fendrick@gmail.com>
2019-10-07 09:13:24 -0400

    Add ngClass and ngFor dir

--- HelloAngular/src/app/app.component.ts
+++ HelloAngular/src/app/app.component.ts
@@ -8,6 +8,15 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'app works!';
   disabledBtn = true;
+  titleColor = 'red';
+
+  toggleTitleColor() {
+    if (this.titleColor === 'red') {
+      this.titleColor = 'blue'
+    } else {
+      this.titleColor = 'red'
+    }
+  }
 
   myMethod(e) {
     this.title = e.target.value
--- HelloAngular/src/app/header/header.component.ts
+++ HelloAngular/src/app/header/header.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
+  navList = ['menu', 'about', 'info', 'contact']
   constructor() { }
 
   ngOnInit() {
